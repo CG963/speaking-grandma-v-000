21e437d433cad7ca783725fdf44026ca5289030a
Christopher Garcia <christopher.garcia08@gmail.com>
2018-12-26 03:23:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,12 @@
 def speak_to_grandma(speak)
-
+if speak == "I LOVE YOU GRANDMA!"
+  "I LOVE YOU TOO PUMPKIN!"
+  
 
 elsif speak == speak.upcase
   "NO, NOT SINCE 1938!"
   
-if speak == "I LOVE YOU GRANDMA!"
-  "I LOVE YOU TOO PUMPKIN!"
-  
+
 else
     "HUH?! SPEAK UP, SONNY!"
   end
